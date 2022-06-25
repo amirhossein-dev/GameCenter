8323bf76542b85cee30b0e72551ca135e3a5de2a
Amir Hossein saffarnia <dev.saffarnia@gmail.com>
2022-06-25 20:56:27 +0430

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  BASE_URL: 'https://api.rawg.io/api',
-  RAW_KEY: '17e64ad755cc44b7b04a48fe50f4ef72',
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  BASE_URL: 'https://api.rawg.io/api',
-  RAW_KEY: '17e64ad755cc44b7b04a48fe50f4ef72',
-};
-//key=17e64ad755cc44b7b04a48fe50f4ef72
